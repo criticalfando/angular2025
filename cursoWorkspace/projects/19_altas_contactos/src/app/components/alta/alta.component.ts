b4f91f518b60f1225e6529685de404ecb1fb2d14
Fernando Sánchez <nandosancheziglesias@gmail.com>
2025-05-26 14:27:44 +0200

    feat: initialize 19_altas_contactos project with core components, routing, and service setup
    - Created AppComponent with basic title and template rendering.
    - Added AppConfig for application configuration and routing.
    - Implemented AltaComponent for adding contacts with a service.
    - Developed ContactosService for HTTP operations related to contacts.
    - Defined Contacto model for contact data structure.
    - Set up unit tests for AppComponent, AltaComponent, and ContactosService.
    - Updated index.html to bootstrap the AltaComponent.
    - Configured TypeScript settings for the application and tests.

---
+++ cursoWorkspace/projects/19_altas_contactos/src/app/components/alta/alta.component.ts
@@ -0,0 +1,25 @@
+import { Contacto } from '../../model/Contacto';
+import { ContactosService } from './../../service/contactos.service';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alta',
+  imports: [],
+  templateUrl: './alta.component.html',
+  styleUrl: './alta.component.css'
+})
+export class AltaComponent {
+  mensaje:string
+  contacto:Contacto=new Contacto()
+
+  constructor(private contactosService:ContactosService){}
+
+  alta(){
+    this.contactosService.alta(this.contacto).subscribe({
+      next: data=>this.mensaje="Contacto agregado",
+      error:  err=>this.mensaje="No se pudo dar de alta el contacto"
+      }
+    )
+  }
+
+}
---
+++ cursoWorkspace/projects/19_altas_contactos/src/app/service/contactos.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, map, Observable } from 'rxjs';
+import { Contacto } from '../model/Contacto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactosService {
+  url:string="http://localhost:3000/contactos"
+  constructor(private http:HttpClient) { }
+
+  alta(contacto:Contacto):Observable<any>{
+    return this.http.post(this.url,contacto)
+  }
+}
